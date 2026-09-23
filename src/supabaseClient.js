3a1767539ada7906e66a4c328fbeebd5fa46c910
edwinj <edwin.junaidi@ogya.co.id>
2026-09-23 23:30:37 +0700

    perubahaan

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,6 +5,7 @@ export const supabase = (import.meta.env.VITE_SUPABASE_URL && import.meta.env.VI
 export async function loginWithSupabase(username, password){
 
   if(!supabase){
+alert("sdsds");
     // fallback dummy jika supabase belum konek
     if(username==='admin' && password==='admin') return {username:'admin', role:'super_admin', nama:'Super Admin ESDM'}
     if(password==='admin') return {username, role:'admin', nama: username}
