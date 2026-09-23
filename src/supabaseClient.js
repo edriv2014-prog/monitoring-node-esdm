fc92386d0cc1245f8d3103409d6c397a7f838b5d
edwinj <edwin.junaidi@ogya.co.id>
2026-09-23 23:18:37 +0700

    perubahaan

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 export const supabase = (import.meta.env.VITE_SUPABASE_URL && import.meta.env.VITE_SUPABASE_ANON_KEY) ? createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY) : null
-
+ 
 export async function loginWithSupabase(username, password){
 
   if(!supabase){
